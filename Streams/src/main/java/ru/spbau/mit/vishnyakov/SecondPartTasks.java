1ba95a50ccca2c98083def2e95d2c554a017153a
Kirill Vishnyakov <intainer7@gmail.com>
2017-12-03 00:23:29 +0300

    Added Streams lab.

---
+++ Streams/src/main/java/ru/spbau/mit/vishnyakov/SecondPartTasks.java
@@ -0,0 +1,86 @@
+package ru.spbau.mit.vishnyakov;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class SecondPartTasks {
+
+    private SecondPartTasks() {}
+
+    // Найти строки из переданных файлов, в которых встречается указанная подстрока.
+    public static List<String> findQuotes(@NotNull List<String> paths, @NotNull CharSequence sequence) throws IOException {
+        Stream<String> st = null;
+        for (String cur : paths) {
+            if (st == null) {
+                st = Files.lines(Paths.get(cur));
+            }
+            else {
+                st = Stream.concat(st, Files.lines(Paths.get(cur)));
+            }
+        }
+        assert st != null;
+        return st.filter(p -> p.contains(sequence)).collect(Collectors.toList());
+    }
+
+    // В квадрат с длиной стороны 1 вписана мишень.
+    // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
+    // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
+    public static double piDividedBy4() {
+        int lim = 100000;
+        Random r = new Random();
+        Stream<Point> stream = Stream.generate(() -> new Point(r.nextDouble(), r.nextDouble())).limit(lim);
+        return stream.filter((p) -> p.getX() * p.getX() + p.getY() * p.getY() <= 1).count() / (double) lim;
+    }
+
+    // Дано отображение из имени автора в список с содержанием его произведений.
+    // Надо вычислить, чья общая длина произведений наибольшая.
+    @SuppressWarnings("ConstantConditions")
+    public static String findPrinter(@NotNull Map<String, List<String>> compositions) {
+        if (compositions.isEmpty()) {
+            return null;
+        }
+        return compositions.entrySet().stream().max(Comparator.comparing(p -> p.getValue().stream().mapToInt(String::length).sum())).get().getKey();
+    }
+
+    // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
+    // Необходимо вычислить, какой товар и в каком количестве надо поставить.
+    public static Map<String, Integer> calculateGlobalOrder(@NotNull List<Map<String, Integer>> orders) {
+        return orders.stream().map(Map::entrySet).flatMap(Collection::stream).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a + b));
+    }
+
+    /**
+     * Class that represents a 2D Point.
+     */
+
+    private static class Point {
+        /**
+         * X and Y coordinates.
+         */
+        double x, y;
+
+        /**
+         * Constructor that receives a point.
+         * @param f -- x coordinate.
+         * @param s -- y coordinate.
+         */
+
+        Point(double f, double s) {
+            x = f;
+            y = s;
+        }
+
+        double getX() {
+            return x;
+        }
+
+        double getY() {
+            return y;
+        }
+    }
+}
